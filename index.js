134f76fb8c5ad455694131bdba2b1013a3bb7e2a
Szewah Chin <swgrleong@gmail.com>
2018-01-03 19:38:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var newArr = kittens.splice()
+  var newArr = kittens.slice()
   newArr.push(name)
   return newArr
 }
